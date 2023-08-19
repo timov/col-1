9921c2025ca299988ce045d997107f26ce579fb0
timov <timovmartin@gmail.com>
2023-08-19 11:52:23 +0300

    zdfsd

--- settings.tsx
+++ settings.tsx
@@ -1,9 +1,9 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "Public", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" }
+  { label: "OG", mintText: "Only selected OG members can mint", buttonLabel: "Mint now!", header: "OGs Mint" },
+  { label: "WL", mintText: "Only selected WL members can mint", buttonLabel: "Mint now!", header: "WLs Mint" },
+  { label: "Public", mintText: "Everyone can mint!", buttonLabel: "Mint now!", header: "Public Mint" }
 ];
 
 //header image in the ui. replace with your own
