f05d1643f97924ded9933af7513d29142d9d99b3
timov <timovmartin@gmail.com>
2023-08-29 23:18:08 +0200

    Sdfd

--- settings.tsx
+++ settings.tsx
@@ -1,9 +1,7 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "OGs", mintText: "Only selected OG members can mint", buttonLabel: "Mint now!", header: "OGs Mint" },
-  { label: "WLs", mintText: "Only selected WL members can mint", buttonLabel: "Mint now!", header: "WLs Mint" },
-  { label: "Public", mintText: "Everyone can mint!", buttonLabel: "Mint now!", header: "Public Mint" }
+  { label: "VIP", mintText: "Only selected VIP members can mint!", buttonLabel: "Mint now!", header: "VIPs Mint" },
 ];
 
 //header image in the ui. replace with your own
