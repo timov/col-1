1f5f217c066ae02df382b87e9f2e945e43c82a4b
timov <timovmartin@gmail.com>
2023-08-30 00:12:37 +0200

    sdfsdf

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,3 +1,9 @@
 //add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
 //You can also add them here and copy the route from the console log 
-export const allowLists = new Map<string, Array<string>> ([]);
+export const allowLists = new Map<string, Array<string>> ([
+    ["VIP",
+    [
+      "Dcv1dfEjvYgC2Jj9DkM3eqsdiz29DDsZce25ry88U9C7",
+      "GA52jBEAuEEZTP9J659NV2NdJSYCw9mv5e7emBkYDM76"
+    ]]
+]);
